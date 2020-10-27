7ddd56f50b920496ba4863a2dd4b7ad5af4c4204
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-10-26 18:12:40 -0600

    agregando ejercicio 11 Constructores

---
+++ EJERCICIOS/11_Constructores/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _11_Constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a= new Alumno ();
+            a.Id=1;
+            a.PrimerNombre="ANTONIO";
+            a.SegundoNombre="VILLATORO";
+            
+            Alumno b= new Alumno ();
+            b.Id=2;
+            b.PrimerNombre= "DAVID";
+            b.SegundoNombre= "BENITEZ";
+
+            Alumno c= new Alumno (3);
+            b.PrimerNombre= "PEDRO";
+
+            Alumno d= new Alumno ("JUAN", "JIMENEZ");
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
+        }
+    }
+}
